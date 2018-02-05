19700bbb932bba080ad967ea6fdc7a4f72fe111b
Reetesh Ranjan <reeteshranjan@piaxis.tech>
2018-02-05 21:08:12 +0530

    v1.2.2 Maintenance Release

--- bin/utils/conf/browsers/saucelabs.js
+++ bin/utils/conf/browsers/saucelabs.js
@@ -12,6 +12,7 @@ const
       },
       osVersion: {
         'OS X': {
+          '10.13': 'High Sierra',
           '10.12': 'Sierra',
           '10.11': 'El Capitan',
           '10.10': 'Yosemite',
